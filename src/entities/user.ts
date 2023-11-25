3343b8ac9612dabf95683d9bf78a93ff325c334a
Andrea Lopez <andrealoga33@gmail.com>
2023-11-25 14:57:23 +0100

    Add entities and User type

---
+++ src/entities/user.ts
@@ -0,0 +1,18 @@
+export type UserLoginData = {
+  email: string;
+  password: string;
+};
+
+export type UserNoId = {
+  name: string;
+  surname: string;
+  email: string;
+  friends: User[];
+  enemies: User[];
+};
+
+export type UserWithId = {
+  id: string;
+};
+
+export type User = UserNoId & UserWithId;
